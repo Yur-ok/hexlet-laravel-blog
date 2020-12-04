3c3ae6d7b0841b943d9dcaf1da43b6b41ab37fee
Yur-ok <materik-r@yandex.ru>
2020-12-04 13:23:26 +0700

    Add flash message after creating new article and add form for creating articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -20,4 +20,31 @@ class ArticleController extends Controller
 
         return view('article.show', compact('article'));
     }
+
+    public function create()
+    {
+        // Передаём в шаблон вновь созданный объект. Он нужен для вывода формы через Form::model
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    public function store(Request $request)
+    {
+        // Проверка введённых данных
+        // Если будут ошибки, то возникнет исключение
+        // Иначе возвращаются данные формы
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:10',
+        ]);
+
+        $article = new Article();
+        // Заполнение статьи данными из формы
+        $article->fill($data);
+        // При ошибках сохранения возникнет исключение
+        $article->save();
+
+        // Редирект на указанный маршрут
+        return redirect('articles')->with('status', 'Article was successfully added!');
+    }
 }
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+    @if(session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+
     <h1>Список статей</h1>
+    <h3><a href="{{route('articles.create')}}">Create new</a></h3>
     @foreach ($articles as $article)
         <h2><a href="{{ route('articles.show', ['id' => $article->id], false) }}">{{$article->title}}</a></h2>
         <div>{{Str::limit($article->body, 200)}}</div>
