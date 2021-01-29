f8bd84c9a4bcf38ef6f6af65e2de648cfdfec64b
Yur-ok <materik-r@yandex.ru>
2021-01-29 10:07:28 +0700

    Change all articles routes on resource route.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,29 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'page.home');
 
-Route::get('about', [PageController::class, 'about']);
+Route::view('about', 'page.about');
 
-Route::get('articles/create', [ArticleController::class, 'create'])
-    ->name('articles.create');
-
-Route::post('articles', [ArticleController::class, 'store'])
-    ->name('articles.store');
-
-Route::get('articles', [ArticleController::class, 'index'])
-    ->name('articles.index');
-
-Route::get('articles/{id}', [ArticleController::class, 'show'])
-    ->name('articles.show');
-
-Route::get('articles/{id}/edit', [ArticleController::class, 'edit'])
-    ->name('articles.edit');
-
-Route::patch('articles/{id}', [ArticleController::class, 'update'])
-    ->name('articles.update');
-
-Route::delete('articles/{id}', [ArticleController::class, 'destroy'])
-    ->name('articles.destroy');
+Route::resource('articles', ArticleController::class);
