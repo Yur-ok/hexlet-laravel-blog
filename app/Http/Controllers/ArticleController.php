97b31bdce2cdc78bbb247aba1eb1479e4389561d
Yur-ok <materik-r@yandex.ru>
2021-01-29 10:03:39 +0700

    Update ArticleController's methods

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,36 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ArticleController extends Controller
 {
-    public function index()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index(): View
     {
-        $articles = Article::paginate(3);
-
+        $articles = Article::paginate(4);
         return view('article.index', compact('articles'));
     }
 
-    public function show($id)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return View
+     */
+    public function create(): View
     {
-        $article = Article::findOrFail($id);
-
-        return view('article.show', compact('article'));
-    }
-
-    public function create()
-    {
-        // Передаём в шаблон вновь созданный объект. Он нужен для вывода формы через Form::model
         $article = new Article();
         return view('article.create', compact('article'));
     }
 
-    public function store(Request $request)
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
     {
-        // Проверка введённых данных
-        // Если будут ошибки, то возникнет исключение
-        // Иначе возвращаются данные формы
         $data = $this->validate(
             $request,
             [
@@ -42,46 +48,75 @@ class ArticleController extends Controller
         );
 
         $article = new Article();
-        // Заполнение статьи данными из формы
         $article->fill($data);
-        // При ошибках сохранения возникнет исключение
         $article->save();
 
-        // Редирект на указанный маршрут
-        return redirect('articles')->with('status', 'Article was successfully added!');
+        return redirect()
+            ->route('articles.index')
+            ->with('status', 'Article was added successfully');
     }
 
-    public function edit($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Article $article
+     * @return View
+     */
+    public function show(Article $article): View
     {
-        $article = Article::findOrFail($id);
+        return view('article.show', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Article $article
+     * @return View
+     */
+    public function edit(Article $article): View
+    {
         return view('article.edit', compact('article'));
     }
 
-    public function update(Request $request, $id)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Article $article
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, Article $article): RedirectResponse
     {
-        $article = Article::findOrFail($id);
         $data = $this->validate(
             $request,
             [
-                'title' => 'required|unique:articles, name,' . $article->id,
+                'title' => 'required|unique:articles, name, ' . $article->id,
                 'body' => 'required|min:100',
             ]
         );
         $article->fill($data);
         $article->save();
+
         return redirect()
-            ->route('articles.update')
-            ->with('status', 'Article is updated!');
+            ->route('articles.update', compact('article'))
+            ->with('status', 'Article is updated');
     }
 
-    public function destroy($id)
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Article $article
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Article $article): RedirectResponse
     {
-        $article = Article::findOrFail($id);
+        $article = Article::findOrFail($article);
         if ($article) {
             $article->delete();
+
+            return redirect()
+                ->route('articles.index')
+                ->with('status', 'Article was deleted');
         }
-        return redirect()
-            ->route('articles.index')
-            ->with('status', 'Article was successfully delete!');
     }
 }
